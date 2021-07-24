bf29ef6818a18c0d82718c63134999fded8666f6
Anthony Gabriele <anthonyjoeseph@gmail.com>
2021-07-24 14:38:18 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,81 @@
+const prop = <
+  Infer,
+>(obj: Infer) => <
+  Path extends Paths<Infer>
+>(...path: Path) => 
+  (path as string[]).map(
+    (p) => p.startsWith('?') ? p.slice(1) : p
+  ).reduce(
+    (acc, cur) => {
+      return acc !== undefined ? acc[cur] : undefined
+    },
+    obj as any
+  ) as AtPath<Infer, Path>
+
+type struct = { three?: { four: string } }
+const testval: struct = {
+  three: {
+    four: '33'
+  }
+}
+
+const aaaa = // <-- correctly inferred as 'string'
+  prop(
+    testval
+  )('?three', 'four') // <-- autocomplete for a variable # of fields
+
+console.log(aaaa)
+
+///////////
+// types //
+//////////
+
+type HasUndesiredKeys<A> = A extends number
+  ? true
+  : A extends string
+    ? true
+    : A extends boolean
+      ? true
+      : A extends Promise<any>
+        ? true
+        : false
+
+type Paths<
+  A, 
+  Prev extends unknown[] = [],
+  Keys = OptKeyof<A>
+> = 
+  HasUndesiredKeys<A> extends false 
+    ? Keys extends unknown
+      ? Unopt<Keys> extends keyof A 
+          ? | [...Prev, Keys] 
+            | Paths<NonNullable<A[Unopt<Keys>]>, [...Prev, Keys]>
+          : never
+      : never
+    : never
+
+type AtPath<
+  A,
+  Args extends unknown[]
+> = Args extends [infer Key]
+  ? Unopt<Key> extends keyof A 
+    ? undefined extends A[Unopt<Key>] ? NonNullable<A[Unopt<Key>]> | undefined : A[Unopt<Key>]
+    : never
+  : Args extends [infer Key, ...infer Rest]
+    ? Unopt<Key> extends keyof A 
+      ? undefined extends A[Unopt<Key>] 
+        ? AtPath<NonNullable<A[Unopt<Key>]>, Rest> | undefined 
+        : AtPath<A[Unopt<Key>], Rest>
+      : never
+    : never
+
+type OptKeyof<A> = {
+  [K in keyof A]-?: undefined extends A[K] 
+    ? K extends string ? `?${K}` : never 
+    : K
+}[keyof A]
+
+type Unopt<K> = K extends `?${infer Key}` ? Key : K
+
+type test = OptKeyof<{ a?: number; b: boolean }>
+type test2 = Unopt<test>
