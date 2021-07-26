287d07d89d32d4f3f30f4d4dae3bb22207b160fb
Anthony Gabriele <anthonyjoeseph@gmail.com>
2021-07-26 00:19:04 -0400

    progress

--- index.ts
+++ index.ts
@@ -1,45 +1,54 @@
-const addOptions = (path: string[], wrapVal: any) => {
-  const numOpts = path.filter(p => p.startsWith).length
-}
-
 const prop = <
   Infer,
 >(obj: Infer) => <
   Path extends Paths<Infer>
->(...path: Path) => 
-  (path as string[]).map(
-    (p) => p.startsWith('?') ? p.slice(1) : p
-  ).reduce(
-    (acc, cur) => {
-      return acc !== undefined ? acc[cur] : undefined
-    },
+>(...path: Path) => {
+  if ((path as string[]).some(p => p.endsWith('?'))) {
+    const a = (path as string[]).reduce(
+      (acc, cur) => {
+        if (acc.isSome) {
+          if (cur.endsWith('?')) {
+            return acc.obj[cur.slice(0, cur.length - 1)] !== undefined
+              ? { obj: acc.obj[cur.slice(0, cur.length - 1)], isSome: true }
+              : { obj: undefined, isSome: false }
+          }
+          return { obj: acc.obj[cur], isSome: true }
+        }
+        return acc
+      },
+      { obj: obj as any, isSome: true }
+    )
+    return (a.isSome
+      ? { _tag: 'Some', value: a.obj }
+      : { _tag: 'None' }) as GiveOpt<AtPath<Infer, Path>, Path>
+  }
+  return (path as string[]).reduce(
+    (acc, cur) => acc[cur],
     obj as any
-  ) as AtPath<Infer, Path>
+  ) as GiveOpt<AtPath<Infer, Path>, Path>
+}
 
 interface Struct { 
   three?: { 
-    four: string 
+    four?: string 
   } 
 }
 const full: Struct = {
-  three: {
-    four: '33'
-  }
+  three: {}
 }
-const nested = // <-- correctly inferred as string | undefined
-  prop(
-    full
-  )('three?', 'four') // <-- autocomplete for a variable # of fields
+const nested = prop(full)('three?', 'four')
+
+console.log(nested)
 
 ///////////
 // types //
 //////////
 
-type Some<A> = {
-  _tag: 'Some',
-  value: A
+interface Some<A> {
+  readonly _tag: 'Some'
+  readonly value: A
 }
-type None = { _tag: 'None' }
+interface None { readonly _tag: 'None' }
 type Option<A> = Some<A> | None
 
 type HasUndesiredKeys<A> = A extends number
@@ -55,36 +64,51 @@ type HasUndesiredKeys<A> = A extends number
 type Paths<
   A, 
   Prev extends unknown[] = [],
-  Keys = OptKeyof<A>
+  Keys = keyof A,
+  OptKeys = OptKeyof<A>
 > = 
   HasUndesiredKeys<A> extends false 
     ? Keys extends unknown
-      ? Unopt<Keys> extends keyof A 
-          ? | [...Prev, Keys] 
-            | Paths<NonNullable<A[Unopt<Keys>]>, [...Prev, Keys]>
-          : never
+      ? | [...Prev, Keys]
+        | (Keys extends keyof A 
+            ? Paths<A[Keys], [...Prev, Keys]> 
+            : never)
+        | (OptKeys extends never 
+          ? never 
+          : | [...Prev, OptKeys]
+            | (Unopt<OptKeys> extends keyof A
+                ? Paths<NonNullable<A[Unopt<OptKeys>]>, [...Prev, OptKeys]> 
+                : never))
       : never
     : never
 
 type AtPath<
   A,
   Args extends unknown[]
-> = Args extends [infer Key]
-  ? Unopt<Key> extends keyof A 
-    ? undefined extends A[Unopt<Key>] ? Option<NonNullable<A[Unopt<Key>]>> : A[Unopt<Key>]
-    : never
-  : Args extends [infer Key, ...infer Rest]
+> = 
+  Args extends [infer Key]
     ? Unopt<Key> extends keyof A 
-      ? undefined extends A[Unopt<Key>] 
-        ? Option<AtPath<NonNullable<A[Unopt<Key>]>, Rest>>
-        : AtPath<A[Unopt<Key>], Rest>
+      ? true extends HasOpt<Key>
+        ? NonNullable<A[Unopt<Key>]>
+        : A[Unopt<Key>]
       : never
-    : never
+    : Args extends [infer Key, ...infer Rest]
+      ? Unopt<Key> extends keyof A 
+        ? true extends HasOpt<Key>
+          ? AtPath<NonNullable<A[Unopt<Key>]>, Rest>
+          : AtPath<A[Unopt<Key>], Rest>
+        : never
+      : never
+
+type GiveOpt<A, Args extends unknown[]> =
+  true extends HasOpt<Args[number]> ? Option<A> : A
 
 type OptKeyof<A> = {
   [K in keyof A]-?: undefined extends A[K] 
     ? K extends string ? `${K}?` : never 
-    : K
+    : never
 }[keyof A]
+
+type HasOpt<K> = K extends `${infer _}?` ? true : never
 
 type Unopt<K> = K extends `${infer Key}?` ? Key : K
