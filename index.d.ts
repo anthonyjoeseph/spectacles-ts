c5422fc181f5ca6ab715405e7028b67a5fa3d4b4
Anthony Gabriele <anthonyjoeseph@gmail.com>
2021-08-24 16:33:03 -0400

    v0.0.2

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-// necessary for tsd
-
-import { get, set, setOption, modify, modifyW, modifyOption, modifyF, upsert, remove, rename } from './dist';
-export { get, set, setOption, modify, modifyW, modifyOption, modifyF, upsert, remove, rename };
