23ba8f12038561ad069505df68c01cbdb615c317
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-03-25 21:53:29 -0400

    improve enforceliteral

--- src/util/sum.ts
+++ src/util/sum.ts
@@ -1,11 +1,15 @@
-type EnforceLiteral<A> = A extends string ? (string extends A ? never : A) : never;
+type EnforceLiteral<A> = string extends A ? never : true;
 
 export type Discriminant<A extends B, B = A> = A extends Record<string, any>
   ? [B] extends [A]
     ? never
     : [keyof A & keyof B] extends [never]
     ? never
-    : EnforceLiteral<keyof A & keyof B>
+    : true extends EnforceLiteral<A[keyof A & keyof B]>
+    ? Extract<keyof A & keyof B, string>
+    : true extends EnforceLiteral<B[keyof A & keyof B]>
+    ? Extract<keyof A & keyof B, string>
+    : never
   : never;
 
 type Members<A, Discriminant extends string> = Extract<A, { [K in Discriminant]: unknown }>[Discriminant];
