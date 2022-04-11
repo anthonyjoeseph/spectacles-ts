10154d6d7a6502304981fd4637ba2d3390e5ad06
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-04-11 14:52:32 -0400

    Document broken, non-recursive paths broken

--- src/util/Paths.ts
+++ src/util/Paths.ts
@@ -3,98 +3,63 @@ import type { Either, Left, Right } from "fp-ts/Either";
 import type { IsAny, IsNull, IsRecord, TupleKeyof } from "./predicates";
 import type { EscapeSpecialChars } from "./segments";
 import type { Cases, Discriminant } from "./sum";
-import { TestNode, TestNode2, TestNodeListOf2 } from "./StructBreadth";
 
 type Operation = "static" | "dynamic" | "upsert";
+
+type a = Paths<{ a: { b: { c: { d: number } } } }>;
 
 interface Rec {
   a?: Rec;
   b?: Rec[];
 }
+// type t2 = Paths<Document>;
+type t3 = Paths<ChildNode>;
+type t19 = Paths<Element>;
+type t7 = Document;
+type t8 = RecordChildren<t7>;
+type t9 = BubbleUp<t7, t7 | t8>;
+type t10 = RecordChildren<t9>;
+type t11 = BubbleUp<t9, t7 | t8 | t10>;
 
-//type t0 = TestNode2;
-type t0 = Element;
-type t1 = BubbleUp<t0, t0 | RecordChildren<t0>>;
-type t2 = RecordChildren<t1>;
-type t3 = BubbleUp<t1, t0 | t2>;
-type t4 = RecordChildren<t3>;
-// type t5 = BubbleUp<t3, t0 | t2 | t4>;
-// type t6 = RecordChildren<t5>;
-// type t7 = BubbleUp<t5, t0 | t2 | t6 | t6>;
-// type t8 = RecordChildren<t7>;
-// type t9 = BubbleUp<t5, t0 | t2 | t6 | t6 | t8>;
-// type t10 = RecordChildren<t9>;
-// type t11 = BubbleUp<t9, t0 | t2 | t6 | t6 | t8 | t10>;
-// type u1 = Paths<TestNode>;
-// type u2 = Paths<Node>;
-// type u3 = ChildNode;
-// type t2 = Paths<ParentNode>;
-// type t3 = Paths<ChildNode>;
-// type t5 = Paths<HTMLElement>;
+type t14 = _Paths<BubbleUp<t7, t7 | t8>, t7 | t8 | RecordChildren<BubbleUp<t7, t7 | t8>>>;
+type t15 = Paths<HTMLElement>;
+// type t14 = RecordChildren<t13>;
+//type t15 = _Paths<t13, t7 | t8 | t10 | t12 | t14, "static">;
 
-declare const u: Specials<t3>;
-u[""];
+export type Paths<A, Op extends Operation = "static"> = _Paths<A, A, Op, Extract<keyof A, string>>;
 
-type a8 = Pick<t3, "parentNode?.childNodes">;
-
-type a9 = _BubbleUp<t8, t0>;
-// type t10 = BubbleUp<t5, Rec>;
-
-export type Paths<A, Op extends Operation = "static"> = _Paths<{ "": A }, { "": A }, Op>;
-
-type Specials<A> = {
-  [K in keyof A as [A[K]] extends [Record<string, any>]
-    ? A[K] extends (...args: any[]) => unknown
-      ? never
-      : K
-    : never]-?: A[K];
-};
-
-type RecordChildren<A> = {
-  [K in keyof A]-?: [A[K]] extends [Record<string, any>]
-    ? A[K] extends (...args: any[]) => unknown
-      ? never
-      : A[K]
-    : never;
-}[keyof A];
-
-type _Paths<
-  A,
-  Recursed,
-  Op extends Operation,
-  Acc extends string = never,
-  It extends unknown[] = []
-> = true extends IsRecord<A>
+type _Paths<A, Recursed, Op extends Operation = "static", Acc extends string = never> = true extends IsRecord<A>
   ? _Paths<
       keyof A extends never ? unknown : BubbleUp<A, Recursed>,
-      Recursed | RecordChildren<A>,
+      Recursed | RecordChildren<BubbleUp<A, Recursed>>,
       Op,
-      Acc | Extract<keyof A, string>,
-      [...It, unknown]
+      Acc | Extract<keyof A, string>
     >
   : Acc;
 
-type NoAnds<A> = { [K in keyof A]: A[K] };
-
-type BubbleUp<A extends Record<string, any>, Recursed> = NoAnds<UnionToIntersection<ValueOf<_BubbleUp<A, Recursed>>>>;
+type BubbleUp<A extends Record<string, any>, Recursed> = UnionToIntersection<ValueOf<_BubbleUp<A, Recursed>>>;
 
 type _BubbleUp<A extends Record<string, any>, Recursed> = {
   [K in keyof A]-?: Match<
     A[K],
     {
       nullable: NonNullable<A[K]> extends Recursed ? never : { [K1 in `${Extract<K, string>}?`]: NonNullable<A[K]> };
-      struct: {
-        [K2 in keyof A[K] as A[K][K2] extends Recursed
-          ? never
-          : `${Extract<K, string>}${Extract<K, string> extends "" ? "" : "."}${EscapeSpecialChars<
-              Extract<K2, string>
-            >}`]: A[K][K2];
-      };
-      tuple: {
-        [K2 in TupleKeyof<A[K]> as A[K][K2] extends Recursed
-          ? never
-          : `${Extract<K, string>}${Extract<K, string> extends "" ? "" : "."}[${Extract<K2, string>}]`]: A[K][K2];
-      };
+      struct: A[K] extends Recursed
+        ? never
+        : {
+            [K2 in keyof A[K] as A[K][K2] extends Recursed
+              ? never
+              : `${Extract<K, string>}${Extract<K, string> extends "" ? "" : "."}${EscapeSpecialChars<
+                  Extract<K2, string>
+                >}`]: A[K][K2];
+          };
+      tuple: A[K] extends Recursed
+        ? never
+        : {
+            [K2 in TupleKeyof<A[K]> as A[K][K2] extends Recursed
+              ? never
+              : `${Extract<K, string>}${Extract<K, string> extends "" ? "" : "."}[${Extract<K2, string>}]`]: A[K][K2];
+          };
       record: A[K][string] extends Recursed
         ? never
         : Record<`${Extract<K, string>}${Extract<K, string> extends "" ? "" : "."}${"[string]" | "{}>"}`, A[K][string]>;
@@ -179,8 +144,57 @@ type Match<
     : Matches["other"]
   : Matches["sum"];
 
+type CanRecurse<A> = unknown extends A
+  ? never
+  : IsAny<A> extends true
+  ? never
+  : [A] extends [never]
+  ? never
+  : A extends (...a: any[]) => unknown
+  ? never
+  : true extends IsNull<A>
+  ? never
+  : [A] extends [Option<unknown>]
+  ? never
+  : [A] extends [Either<unknown, unknown>]
+  ? never
+  : [A] extends [readonly unknown[]]
+  ? TupleKeyof<A> extends never
+    ? never
+    : true
+  : string extends keyof A
+  ? never
+  : true extends IsRecord<A>
+  ? true
+  : never;
+
 // Credit to Stefan Baumgartner
 // https://fettblog.eu/typescript-union-to-intersection/
 type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (x: infer R) => any ? R : never;
 
 type ValueOf<A> = A[keyof A];
+
+type Specials<A> = {
+  [K in keyof A as [A[K]] extends [Record<string, any>]
+    ? A[K] extends (...args: any[]) => unknown
+      ? never
+      : K
+    : never]-?: A[K];
+};
+type NonSpecials<A> = {
+  [K in keyof A as [A[K]] extends [Record<string, any>]
+    ? A[K] extends (...args: any[]) => unknown
+      ? K
+      : never
+    : K]-?: A[K];
+};
+
+type RecordChildren<A> = {
+  [K in keyof A]-?: [A[K]] extends [Record<string, any>]
+    ? A[K] extends (...args: any[]) => unknown
+      ? never
+      : A[K]
+    : never;
+}[keyof A];
+
+type NoAnds<A> = { [K in keyof A]: A[K] };
